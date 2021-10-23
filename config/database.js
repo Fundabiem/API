75a3ec6523d1754098cf8a0f7a65546b89acee58
EduardTynoLucero <bryaneduardlucero@gmail.com>
2021-10-23 06:46:25 -0600

    cambio credencial

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '35.192.128.154'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'fundabiemdb'),
-        username: env('DATABASE_USERNAME', 'fundabiemdb'),
-        password: env('DATABASE_PASSWORD', '123'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
